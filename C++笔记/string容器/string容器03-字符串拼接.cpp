155bdec2474b0f2ff0dcc9f62ae16715451b66f7
徐国彩 <3519610554@qq.com>
2022-08-30 20:51:24 +0800

    更新8.30

--- C++笔记/string容器/string容器03-字符串拼接.cpp
+++ C++笔记/string容器/string容器03-字符串拼接.cpp
@@ -1,10 +1,64 @@
-//string 字符串
+//string 字符串拼接
 # include <iostream>
 using namespace std;
+# include <string>
 
-int naim() {
+//功能描述：实现在字符串末尾拼接字符
+
+/*
+函数原型：
+string& operator+=(const char* str);                  重载+=操作符
+string& operator+=(const char c);                     重载+=操作符
+string& operator+=(const string& str);                重载+=操作符
+string& append(const char *s);                        把字符串s连接到当前字符串结尾
+string& append(const char *s, int n);                 把字符串s的前n个字符连接到当前字符串结尾
+string& append(const string &s);                      同operator+=(const string& str);
+string& append(const string &s, int pos, int n);      字符串s中从pos开始的n个字符串连接到字符串结尾
+*/
+
+void test01() {
+
+	string str1 = "我";
+	str1 += "爱玩游戏";
+	cout << "str1 = " << str1 << endl;
+
+	str1 += ':';
+
+	cout << "str1 = " << str1 << endl;
+
+	string str2 = "LoL DNF";
+	str1 += str2;
+
+	cout << "str1 = " << str1 << endl;
+
+	string str3 = "I";
+	str3.append(" Love ");
+
+	cout << "str3 = " << str3 << endl;
+
+	str3.append("game abcd", 5);
+
+	cout << "str3 = " << str3 << endl;
+
+	// I Love game LoL DNF
+	//str3.append(str2);
+
+	//I Love game LoL
+	//str3.append(str2, 0, 3);
+
+	//I Love game DNF
+	str3.append(str2, 4, 6);
+
+	cout << "str3 = " << str3 << endl;
+}
+
+int main() {
+
+	test01();
 
 	system("pause");
 
 	return 0;
 }
+
+//总结：字符串拼接的重载版本越多， 初学阶段记住几种即可
