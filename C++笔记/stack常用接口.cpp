b3897ac0792902627bff837285d8172433b92fee
徐国彩 <3519610554@qq.com>
2022-09-06 17:14:57 +0800

    更新9.6

---
+++ C++笔记/stack常用接口.cpp
@@ -0,0 +1,39 @@
+//stack常用接口
+# include <iostream>
+using namespace std;
+# include <stack>
+
+//栈stack容器
+void test01() {
+
+	//特点：符合先进后出数据结构
+	stack<int>s;
+
+	//入栈
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+
+	//只要栈不为空，查看栈顶，并且执行出栈操作
+	cout << "栈的大小:" << s.size() << endl;
+	while (!s.empty()) {
+
+		//查看栈顶元素
+		cout << "栈顶元素为：" << s.top() << endl;
+
+		//出栈
+		s.pop();
+	}
+
+	cout << "栈的大小:" << s.size() << endl;
+}
+
+int main() {
+
+	test01();
+
+	system("Pause");
+
+	return 0;
+}
